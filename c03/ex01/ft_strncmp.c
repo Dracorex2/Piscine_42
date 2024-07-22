a12580b3fbee99add51bee3f2f2c136c6c9e6a3b
Lucas Mansart <lucmansa@c3r3p6.42perpignan.fr>
2024-07-22 11:07:21 +0200

    modif c03

---
+++ c03/ex01/ft_strncmp.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lucmansa <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/22 10:46:24 by lucmansa          #+#    #+#             */
+/*   Updated: 2024/07/22 11:06:41 by lucmansa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include<stdio.h>
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i] && i < n - 1)
+		i++;
+	return (s1[i] - s2[i]);
+}
+/*
+int main(void)
+{
+	char    str1[] = "abcc";
+	char    str2[] = "abcd";
+	unsigned int i;
+
+	i = 3;
+	printf("%d", ft_strncmp(str1, str2, i));
+}
+*/
