115291d58861095e3f6c75dbad70bfa2fceaa169
Dracorex2 <lucas.m3166@gmail.com>
2024-08-07 21:53:36 +0200

    racoursicement conv base

--- c07/ex04/ft_convert_base.c
+++ c07/ex04/ft_convert_base.c
@@ -6,7 +6,7 @@
 /*   By: lucmansa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/06 14:15:48 by lucmansa          #+#    #+#             */
-/*   Updated: 2024/08/07 16:10:38 by lucmansa         ###   ########.fr       */
+/*   Updated: 2024/08/07 21:50:35 by lucmansa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int	ft_strlen(char *str)
-{
-	int	size;
-
-	size = 0;
-	if (!str)
-		return (0);
-	while (str[size] != '\0')
-		size++;
-	return (size);
-}
-
 int	base_check(char *str)
 {
 	int	i;
 	int	j;
 
 	i = -1;
-	if (ft_strlen(str) == 0 || ft_strlen(str) == 1)
+	while (str[++i])
+		i++;
+	if (i < 2)
 		return (0);
+	i = -1;
 	while (str[++i])
 	{
+
 		if ((str[i] == '+' || str[i] == '-' || str[i] == ' ')
 			|| (str[i] >= '\t' && str[i] <= '\r'))
 			return (0);
@@ -44,7 +36,7 @@ int	base_check(char *str)
 			if (str[i] == str[j])
 				return (0);
 	}
-	return (1);
+	return (i);
 }
 
 int	ft_comp(char c, char *base)
@@ -63,25 +55,20 @@ void	ft_putnbr_base(int nbr, char *base, char *res, int *i)
 	int long	nbl;
 	int			b;
 
-	b = ft_strlen(base);
+	b = base_check(base);
 	nbl = nbr;
-	if (base_check(base) == 0)
-		return ;
-	else
+	if (nbl < 0)
 	{
-		if (nbl < 0)
-		{
-			res[++(*i)] = '-';
-			nbl *= -1;
-		}
-		if (nbl >= b)
-		{
-			ft_putnbr_base(nbl / b, base, res, i);
-			ft_putnbr_base(nbl % b, base, res, i);
-		}
-		else
-			res[++(*i)] = base[nbl];
+		res[++(*i)] = '-';
+		nbl *= -1;
 	}
+	if (nbl >= b)
+	{
+		ft_putnbr_base(nbl / b, base, res, i);
+		ft_putnbr_base(nbl % b, base, res, i);
+	}
+	else
+		res[++(*i)] = base[nbl];
 }
 
 char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
@@ -92,7 +79,7 @@ char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
 	char	*res;
 
 	res = malloc(sizeof(char) * 36);
-	if (!base_check(base_from) || !base_check(base_to))
+	if (base_check(base_from) < 2 || base_check(base_to) < 2)
 		return (0);
 	nb = 0;
 	i = 0;
@@ -106,7 +93,7 @@ char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
 		i++;
 	}
 	while (ft_comp(nbr[i], base_from) != -1)
-		nb = nb * ft_strlen(base_from) + (ft_comp(nbr[i++], base_from));
+		nb = nb * base_check(base_from) + (ft_comp(nbr[i++], base_from));
 	i = -1;
 	ft_putnbr_base((nb * sign), base_to, res, &i);
 	res[++i] = 0;
@@ -115,10 +102,10 @@ char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
 /*
 int main()
 {
-	char    str[] = "42";
+	char    str[] = "	-2147483648";
 	char    *res;
 
-	res = ft_convert_base(str, "0123456789", "0123456789abcdef");
+	res = ft_convert_base(str, "0123456789", "01");
 	printf("%s", res);
 }
 */
