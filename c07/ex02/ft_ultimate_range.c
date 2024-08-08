cac058b4cb969c35057b7a4c20375e83d2169452
Dracorex2 <lucas.m3166@gmail.com>
2024-08-08 11:30:38 +0200

    exxx

--- c07/ex02/ft_ultimate_range.c
+++ c07/ex02/ft_ultimate_range.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_ultimate_range.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lucmansa <lucmansa@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lucmansa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 14:49:10 by lucmansa          #+#    #+#             */
-/*   Updated: 2024/08/08 10:28:29 by lucmansa         ###   ########.fr       */
+/*   Updated: 2024/08/08 11:30:20 by lucmansa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,38 +14,40 @@
 
 int ft_ultimate_range(int **range, int min, int max)
 {
-    int i;
-    int len;
+	int	i;
+	int	len;
 
-    if (min < max)
-    {
-            return (0);
-        len = max - min;
-        i = 0;
-        *range = malloc(sizeof(int) * len);
-        if (range == NULL)
-            return (0);
-        while (min < max)
-        {
-            (*range)[i] = min;
-            min++;
-            i++;
-        }
-        return (len);
-    }
-    return (0);
+	if (min > max)
+	{
+		*range = NULL;
+		return (0);
+	}
+	len = max - min;
+	i = 0;
+	*range = malloc(sizeof(int) * len);
+	if (range == NULL)
+		return (-1);
+	while (min < max)
+	{
+		(*range)[i] = min;
+		min++;
+		i++;
+	}
+	return (len);
 }
-
+/*
 #include <stdio.h>
 int	main(void)
 {
 	int	*res;
-    int i; 
+	int	i; 
+	int	size;
 
-    i = -1;
-	ft_ultimate_range(&res, 2, 0);
-    while (res[++i] != 100)
-	    printf("%i\n", res[i]);
-    free(res);
+	i = 0;
+	size = ft_ultimate_range(&res, 0, 10);
+	while (i < size)
+		printf("%i\n", res[i++]);
+	free(res);
 	return (0);
 }
+*/
